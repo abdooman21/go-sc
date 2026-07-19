e8f27fe30fd559b4b9e0e57275f3cb87f83297db
abdooman21 <abdos3new55@gmail.com>
2026-07-19 04:09:21 +0300

    added 2 test cases and urls fetcher

--- html.go
+++ html.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/url"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -42,4 +43,26 @@ func getFirstParagraphFromHTML(html string) string {
 	}
 
 	return ""
+}
+
+func getURLsFromHTML(htmlBody string, baseURL *url.URL) ([]string, error) {
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlBody))
+	if err != nil {
+		return nil, err
+	}
+	urls := []string{}
+	doc.Find("a[href]").Each(func(_ int, s *goquery.Selection) {
+		link, notnil := s.Attr("href")
+		if notnil {
+
+			res, err := url.Parse(link)
+			if err != nil {
+				return
+			}
+			link := baseURL.ResolveReference(res)
+
+			urls = append(urls, link.String())
+		}
+	})
+	return urls, nil
 }
--- normalize_url_test.go
+++ normalize_url_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"net/url"
+	"reflect"
+	"testing"
+)
 
 func TestNormalizeURL(t *testing.T) {
 	tests := []struct {
@@ -53,5 +57,26 @@ func TestGetFirstParagraphFromHTMLMainPriority(t *testing.T) {
 
 	if actual != expected {
 		t.Errorf("expected %q, got %q", expected, actual)
+	}
+}
+
+func TestGetURLsFromHTMLAbsolute(t *testing.T) {
+	inputURL := "https://crawler-test.com"
+	inputBody := `<html><body><a href="https://crawler-test.com"><span>Boot.dev</span></a></body></html>`
+
+	baseURL, err := url.Parse(inputURL)
+	if err != nil {
+		t.Errorf("couldn't parse input URL: %v", err)
+		return
+	}
+
+	actual, err := getURLsFromHTML(inputBody, baseURL)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	expected := []string{"https://crawler-test.com"}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("expected %v, got %v", expected, actual)
 	}
 }
